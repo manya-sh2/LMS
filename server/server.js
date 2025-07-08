04b8249beaad018b2eb2156ff86d28ee1aeb8ea7
manya_sh2 <2005.sharma.manya@gmail.com>
2025-07-08 17:44:12 +0530

    vercel json added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './configs/mogodb.js'
+import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 
 //initialize express
@@ -15,11 +15,12 @@ app.use(cors())
 
 //routes
 app.get('/', (req, res)=> res.send("API Working"))
+
 app.post('/clerk', express.json(), clerkWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
